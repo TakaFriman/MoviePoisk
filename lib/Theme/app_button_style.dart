8019d103a07acdcaff021ddf986ec3c18850857f
TakaFriman <geor.tebiev@mail.ru>
2023-06-08 15:15:04 +0300

    Fix

--- lib/Theme/app_button_style.dart
+++ lib/Theme/app_button_style.dart
@@ -6,7 +6,7 @@ abstract class AppButtonStyle {
       Colors.black,
     ),
     textStyle: MaterialStateProperty.all(
-      const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+      const TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
     ),
   );
 }
